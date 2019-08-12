186f17d8b69464dad610c1558a30e617106892ee
CountChu <visualge@gmail.com>
2019-08-13 01:30:05 +0800

    - Update README.md and usage

--- GitChangeAuthor.py
+++ GitChangeAuthor.py
@@ -4,8 +4,8 @@
 #
 # FUNCTIONAL DESCRIPTION.
 #       The Python application GitChangeAuthor.py changes author name and email
-#       of all commitments in a current branch , but keeps all timestamp in the branch 
-#       by doing the Git amend and rebase commands. 
+#       of all commitments in a current branch , but keeps all timestamp in the branch
+#       by doing the Git amend and rebase commands.
 #
 # NOTICE.
 #       Author: visualge@gmail.com (CountChu)
@@ -31,50 +31,68 @@ import Util
 #
 # Build argument parser and return it.
 #
-    
+
 def buildArgParser():
 
+    desc = '''
+The app changes an author but keeps timestamps for the current branch.
+
+Usage:
+
+    1. Go to your local repository.
+
+    2. Run GitChangeAuthor.py to change the author.
+
+        $ python HOME\GitChangeAuthor.py --name CountChu --email visualge@gmail.com
+            HOME is a path that contains GitChangeAuthor.py
+
+    3. Push your changes into the remote repository.
+    
+        $ git push --force
+'''
+
     parser = argparse.ArgumentParser(
-                description='Build ...')
-                
+                formatter_class=argparse.RawTextHelpFormatter,
+                description=desc)
+
     #
     # Standard arguments
     #
-                
+
     parser.add_argument(
-            "-v", 
-            dest="verbose", 
-            action='store_true',    
-            help="Verbose log") 
-            
+            "-v",
+            dest="verbose",
+            action='store_true',
+            help="Verbose log")
+
     parser.add_argument(
             '--log',
             dest='logFn',
-            help='A name of a log file.')             
-            
+            help='A name of a log file.')
+
     #
     # Anonymous arguments.
     #
-                
+
     #
     # Specific arguments
-    #     
-    
+    #
+
     parser.add_argument(
             '--name',
             dest='name',
             required=True,
-            help='A user name') 
+            help='Change the user.name of the repository.')
 
     parser.add_argument(
             '--email',
             dest='email',
             required=True,
-            help='A user email') 
-            
+            help='Change the user.email of the repository')
+
     return parser
-    
-def readConfig(jsonFn):   
+
+def readConfig(jsonFn):
     if not os.path.exists(jsonFn):
         return None
 
@@ -83,100 +101,100 @@ def readConfig(jsonFn):
     jsonStr = ''.join(lines)
     jsonObj = json.loads(jsonStr)
     f.close()
-    
-    return jsonObj     
-    
-def main():    
+
+    return jsonObj
+
+def main():
 
     #
     # Parse arguments
     #
-    
+
     args = buildArgParser().parse_args()
-    
+
     #
     # Enable log if -v
     #
-    
+
     if args.verbose:
         logging.basicConfig(level=logging.DEBUG, format='%(message)s')
     logging.info(args)
-    
+
     #
     # Check arguments.
     #
-    
+
     #
     # Open a log file if --log
     #
-    
+
     if args.logFn != None:
         logF = open(args.logFn, 'w')
-        
+
     #
     # Read config.
     #
-    
+
     jsonFn = 'GitChangeAuthor.json'
     jsonObj = readConfig(jsonFn)
-    
+
     #
     # Here is core function.
     #
-    
-    logList = Util.getLogs()    
-        
-    cmd = ['git', 'config', 'user.name', args.name]    
+
+    logList = Util.getLogs()
+
+    cmd = ['git', 'config', 'user.name', args.name]
     lines = Util.command(cmd, True)
     Util.printLines(lines)
-    
-    cmd = ['git', 'config', 'user.email', args.email]    
-    lines = Util.command(cmd, True)    
+
+    cmd = ['git', 'config', 'user.email', args.email]
+    lines = Util.command(cmd, True)
     Util.printLines(lines)
-    
+
     cmd = ['git', 'rev-parse', '--abbrev-ref', 'HEAD']
-    lines = Util.command(cmd, True)    
+    lines = Util.command(cmd, True)
     Util.printLines(lines)
     branch = lines[0]
-    
+
     for i in reversed(range(len(logList))):
         log = logList[i]
         print(i, log)
-        
+
         date = log[1]
-        
+
         logList2 = Util.getLogs()
         hash = logList2[i][0]
-        
+
         cmd = ['git', 'checkout', '-b', 'temp', hash]
-        lines = Util.command(cmd, True)    
-        Util.printLines(lines)        
-        
+        lines = Util.command(cmd, True)
+        Util.printLines(lines)
+
         os.environ['GIT_COMMITTER_DATE'] = '"%s"' % date
         print('set GIT_COMMITTER_DATE = "%s"' % date)
-        
+
         cmd = ['git', 'commit', '--amend', '--date=%s' % date, '--reset-author', '--no-edit']
-        lines = Util.command(cmd, True, shell=True, env=dict(os.environ))    
+        lines = Util.command(cmd, True, shell=True, env=dict(os.environ))
         Util.printLines(lines)
 
         cmd = ['git', 'checkout', branch]
-        lines = Util.command(cmd, True)    
+        lines = Util.command(cmd, True)
         Util.printLines(lines)
-        
+
         cmd = ['git', 'rebase', hash, '--onto', 'temp']
-        lines = Util.command(cmd, True)    
+        lines = Util.command(cmd, True)
         Util.printLines(lines)
-    
+
         cmd = ['git', 'branch', '-d', 'temp']
-        lines = Util.command(cmd, True)    
+        lines = Util.command(cmd, True)
         Util.printLines(lines)
-        
-    Util.getLogs()    
-    
+
+    Util.getLogs()
+
     #
     # Close the log file if --log
     #
-    
+
     if args.logFn != None:
         logF.close()
 
--- README.md
+++ README.md
@@ -1,26 +1,31 @@
 # GitChangeAuthor
-The Python application GitChangeAuthor.py changes author, but keeps timestamp for the current branch.
+The Python application [GitChangeAuthor.py](GitChangeAuthor.py) changes author, but keeps timestamp for the current branch.
 
 It changes author names and emails of all commitments in a current branch, but keeps all timestamp in the branch by doing the Git amend and rebase commands
 
 ## Usage
+```
+usage: GitChangeAuthor.py [-h] [-v] [--log LOGFN] --name NAME --email EMAIL
 
-1. Go to your local repository.
+The app changes an author but keeps timestamps for the current branch.
 
-For examples:
-```
-D:\DashboardPusher
-```
+Usage:
 
-2. Run GitChangeAuthor.py with to change the author
-```
-$ python $HOME\GitChangeAuthor.py --name CountChu --email visualge@gmail.com
-```
-- $Home is a path that contains GitChangeAuthor.py 
-- --name changes the user.name for the repository
-- --email changes the user.email for the repository
+    1. Go to your local repository.
 
-3. Push your changes into the remote repository.
-```
-$ git push --force
+    2. Run GitChangeAuthor.py to change the author.
+
+        $ python HOME\GitChangeAuthor.py --name CountChu --email visualge@gmail.com
+            HOME is a path that contains GitChangeAuthor.py
+
+    3. Push your changes into the remote repository.
+
+        $ git push --force
+
+optional arguments:
+  -h, --help     show this help message and exit
+  -v             Verbose log
+  --log LOGFN    A name of a log file.
+  --name NAME    Change the user.name of the repository.
+  --email EMAIL  Change the user.email of the repository
 ```
